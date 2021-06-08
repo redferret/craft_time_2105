70ebb78bbf73b47d55f2e3f95cbf05f81d087c2a
Richard <rdesilvey@gmail.com>
2021-06-08 17:33:39 -0600

    Implement method

--- lib/person.rb
+++ lib/person.rb
@@ -10,4 +10,16 @@ class Person
   def add_supply(craft_item, quantity)
     @supplies[craft_item] += quantity
   end
+
+  def can_build?(craft)
+    keys = craft.supplies_required.keys
+
+    result = keys.map do |key|
+      if supplies.has_key?(key)
+        supplies[key] >= craft.supplies_required[key]
+      end
+    end
+    
+    not(result.include?(false))
+  end
 end

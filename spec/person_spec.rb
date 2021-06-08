fcdd556b92435cebf8bbaf2d591db22150d39fd4
Richard <rdesilvey@gmail.com>
2021-06-08 17:33:34 -0600

    Write test for can_build?

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -35,5 +35,30 @@ RSpec.describe Person do
         expect(person.supplies).to eq expected_supplies
       end
     end
+
+    describe '#can_build?' do 
+      before :each do
+        supplies_1 = {fabric: 5, scissors: 2}
+        supplies_2 = {fabric: 6, scissors: 0}
+
+        @person_1 = Person.new({name: 'Hector', interests: ['sewing', 'drawing']})
+        @person_2 = Person.new({name: 'Lana', interests: ['sewing', 'drawing']})
+
+        allow(@person_1).to receive(:supplies).and_return(supplies_1)
+        allow(@person_2).to receive(:supplies).and_return(supplies_2)
+
+        @craft_1 = instance_double('Craft', supplies_required: {fabric: 5, scissors: 1})
+      end
+
+      it 'returns true if the person has enough supplies required' do
+        actual_result = @person_1.can_build?(@craft_1)
+        expect(actual_result).to eq true
+      end
+      
+      it 'returns false if the person does not have enough supplies' do
+        actual_result = @person_2.can_build?(@craft_1)
+        expect(actual_result).to eq false
+      end
+    end
   end
 end
